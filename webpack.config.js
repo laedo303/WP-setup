9e4f052a54155eb428da1764df505a94dd0abca9
Konstantin <laedo303@mail.ru>
2022-07-03 18:19:40 +0500

    add-image-compression

--- webpack.config.js
+++ webpack.config.js
@@ -62,6 +62,31 @@ module.exports = {
       },
       {
         test: /\.(jpe?g|png|webp|gif|svg)$/i,
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          },
+        ],
         type: 'asset/resource',
         generator: {
           filename: 'pic/[name][ext]',
